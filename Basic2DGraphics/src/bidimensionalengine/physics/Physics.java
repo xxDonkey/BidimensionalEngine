3b425ddad48e0a970df8c8411515471f8e9abf9b
Dylan R. <d.raiff@icloud.com>
2020-01-16 19:31:06 -0500

    created Physics class

---
+++ Basic2DGraphics/src/bidimensionalengine/physics/Physics.java
@@ -0,0 +1,14 @@
+package bidimensionalengine.physics;
+
+public final class Physics
+{
+
+	/**
+	 * Private constructer to disallow instantiation of the class.
+	 */
+	private Physics()
+	{}
+
+	// TODO: implement static phys methods
+
+}
