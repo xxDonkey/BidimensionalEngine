0b955a4d536a07d655e3850e0543458d04ef27de
Dylan R. <d.raiff@icloud.com>
2020-01-20 16:25:49 -0500

    created GameCore

---
+++ BidimensionalEngine/src/bidimensionalengine/core/GameCore.java
@@ -0,0 +1,13 @@
+package bidimensionalengine.core;
+
+/**
+ * Your {@code Main} or {@code Run} class should implement this interface. <br>
+ * This interface is not required, but you will have to manually call the Window
+ * constructor, <b>it would be much more difficult</b>.
+ * 
+ * @author Dylan Raiff
+ */
+public interface GameCore
+{
+
+}
