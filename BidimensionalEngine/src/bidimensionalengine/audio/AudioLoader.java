98c58facc4c7d733f3650fb89a2e97947571cdad
Dylan R. <d.raiff@icloud.com>
2020-01-20 18:35:24 -0500

    added private constructor

--- BidimensionalEngine/src/bidimensionalengine/audio/AudioLoader.java
+++ BidimensionalEngine/src/bidimensionalengine/audio/AudioLoader.java
@@ -1,9 +1,17 @@
 package bidimensionalengine.audio;
 
+import java.util.ArrayList;
+
 /**
  * @author Dylan Raiff
  */
 public class AudioLoader
 {
+	/**
+	 * Private constructer to disallow instantiation of the class.
+	 */
+	private AudioLoader()
+	{}
 
+	private static ArrayList<Audio> loadedAudio = new ArrayList<Audio>();
 }
