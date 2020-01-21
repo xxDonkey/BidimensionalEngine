b8ab2f874375f2b16a03eda850653126b72dc42e
Dylan R. <d.raiff@icloud.com>
2020-01-20 19:03:18 -0500

    added audio data

--- BidimensionalEngine/src/bidimensionalengine/audio/Audio.java
+++ BidimensionalEngine/src/bidimensionalengine/audio/Audio.java
@@ -1,9 +1,45 @@
 package bidimensionalengine.audio;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.Clip;
+
 /**
  * @author Dylan Raiff
+ * @version 1.0
  */
 public class Audio
 {
+	/**
+	 * Name of the audio.
+	 */
+	private String name;
 
+	/**
+	 * Object holding audio data.
+	 */
+	private Clip audio;
+
+	/**
+	 * Stream of the {@code Clip}.
+	 */
+	private AudioInputStream stream;
+
+	/**
+	 * Creates a new {@code Audio} object and assigns variables.
+	 * 
+	 * @param name   name of the audio
+	 * @param audio  {@code Clip} holding audio information
+	 * @param stream {@code AudioInputStream} of the audio's clip
+	 */
+	public Audio(String name, Clip audio, AudioInputStream stream)
+	{
+		this.name = name;
+		this.audio = audio;
+		this.stream = stream;
+	}
+
+	/* Access methods */
+
+	public String getName()
+	{ return name; }
 }
