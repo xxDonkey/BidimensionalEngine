2236e5e6404be1843081d8b4694a09dcb0558db5
Dylan R. <d.raiff@icloud.com>
2020-01-20 19:03:29 -0500

    implemented audio loading

--- BidimensionalEngine/src/bidimensionalengine/audio/AudioLoader.java
+++ BidimensionalEngine/src/bidimensionalengine/audio/AudioLoader.java
@@ -1,9 +1,22 @@
 package bidimensionalengine.audio;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
+import bidimensionalengine.core.Window;
 
 /**
  * @author Dylan Raiff
+ * @version 1.0
  */
 public class AudioLoader
 {
@@ -13,5 +26,37 @@ public class AudioLoader
 	private AudioLoader()
 	{}
 
+	/**
+	 * List of all loaded audio files.
+	 */
 	private static ArrayList<Audio> loadedAudio = new ArrayList<Audio>();
+
+	public static boolean loadAudio(String filename)
+	{
+		if (Window.getInstance() == null)
+			return false;
+
+		File audioFile = new File(Window.getAssetDirectory() + "audio/" + filename);
+		AudioInputStream stream = null;
+		AudioFormat format = null;
+		DataLine.Info info = null;
+		Clip clip = null;
+
+		try
+		{
+			stream = AudioSystem.getAudioInputStream(audioFile);
+			format = stream.getFormat();
+			info = new DataLine.Info(Clip.class, format);
+
+			clip = (Clip) AudioSystem.getLine(info);
+			System.out.println(clip);
+			loadedAudio.add(new Audio(filename, clip, stream));
+		}
+		catch (UnsupportedAudioFileException | IOException | LineUnavailableException e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
 }
