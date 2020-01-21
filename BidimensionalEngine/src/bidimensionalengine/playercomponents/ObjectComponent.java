78c21e0fc87e739b32c26fde532abf64f3e56abb
Dylan R. <d.raiff@icloud.com>
2020-01-20 19:32:25 -0500

    added javadoc comments

--- BidimensionalEngine/src/bidimensionalengine/playercomponents/ObjectComponent.java
+++ BidimensionalEngine/src/bidimensionalengine/playercomponents/ObjectComponent.java
@@ -8,15 +8,29 @@ import bidimensionalengine.datastructs.GameObject;
  */
 public abstract class ObjectComponent
 {
+	/**
+	 * Called every tick by the {@code GameLoop}.
+	 */
 	public abstract void update();
 
+	/**
+	 * {@code GameObject} this component is attached to.
+	 */
 	protected GameObject gameObject;
 
+	/**
+	 * Creates a new component and attaches it to the given game object.
+	 * 
+	 * @param gameObject parent, or attached object
+	 */
 	public ObjectComponent(GameObject gameObject)
 	{ this.gameObject = gameObject; }
 
 	/* Access methods */
 
+	/**
+	 * @return the {@code GameObject} this component is attached to
+	 */
 	public GameObject getParent()
 	{ return gameObject; }
 
