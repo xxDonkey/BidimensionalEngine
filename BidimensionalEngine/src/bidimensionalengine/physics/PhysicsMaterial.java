a5c5c65ecb4dcab545d8556611d4c209e207d959
Dylan R. <d.raiff@icloud.com>
2020-01-20 21:47:39 -0500

    added slight implementation

--- BidimensionalEngine/src/bidimensionalengine/physics/PhysicsMaterial.java
+++ BidimensionalEngine/src/bidimensionalengine/physics/PhysicsMaterial.java
@@ -1,6 +1,13 @@
 package bidimensionalengine.physics;
 
+/**
+ * @author Dylan Raiff
+ * @version 1.0
+ */
 public class PhysicsMaterial
 {
-
+	/**
+	 * How bouncy the collider that this {@code PhysicsMaterial} is.
+	 */
+	public double bounciness = 1;
 }
