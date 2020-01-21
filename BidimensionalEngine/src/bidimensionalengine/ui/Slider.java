52a76b3b9828e22af9c196e448048fe5d65bd48a
Dylan R. <d.raiff@icloud.com>
2020-01-21 09:03:15 -0500

    created slider class

---
+++ BidimensionalEngine/src/bidimensionalengine/ui/Slider.java
@@ -0,0 +1,19 @@
+package bidimensionalengine.ui;
+
+/**
+ * @author Dylan Raiff
+ * @version 1.0
+ */
+public class Slider extends UIElement
+{
+
+	/**
+	 * Creates a new slider.
+	 * 
+	 * @param name   name of the slider
+	 * @param parent parent of the slider
+	 */
+	public Slider(String name, Container parent)
+	{ super(name, parent); }
+
+}
