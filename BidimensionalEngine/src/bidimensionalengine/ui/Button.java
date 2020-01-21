99527eacf6eba72fb73a33f626dec3f1a40f664a
Dylan R. <d.raiff@icloud.com>
2020-01-20 19:57:46 -0500

    created button class

---
+++ BidimensionalEngine/src/bidimensionalengine/ui/Button.java
@@ -0,0 +1,12 @@
+package bidimensionalengine.ui;
+
+/**
+ * @author Dylan Raiff
+ * @version 1.0
+ */
+public class Button extends UIElement
+{
+
+	public Button(String name, Container parent)
+	{ super(name, parent); }
+}
