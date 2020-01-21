8bccb753139a7b32f8d209277af51cef6798bd22
Dylan R. <d.raiff@icloud.com>
2020-01-20 21:35:37 -0500

    created collider

---
+++ BidimensionalEngine/src/bidimensionalengine/physics/colliders/Collider.java
@@ -0,0 +1,12 @@
+package bidimensionalengine.physics.colliders;
+
+import bidimensionalengine.datastructs.Vector2;
+
+/**
+ * @author Dylan Raiff
+ * @version 1.0
+ */
+public class Collider
+{
+	protected Vector2[] bounds;
+}
