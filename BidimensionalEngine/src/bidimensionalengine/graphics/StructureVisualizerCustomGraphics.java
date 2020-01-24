0981dd3087cc4bfc157abcb53df50c6f0a32ac95
Dylan R. <d.raiff@icloud.com>
2020-01-24 17:05:19 -0500

    created file

---
+++ BidimensionalEngine/src/bidimensionalengine/graphics/StructureVisualizerCustomGraphics.java
@@ -0,0 +1,12 @@
+package bidimensionalengine.graphics;
+
+import java.awt.Component;
+
+/**
+ * @author Dylan Raiff
+ * @version 1.0
+ */
+public class StructureVisualizerCustomGraphics extends Component
+{
+
+}
