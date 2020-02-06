2252b88a9e8ff05e0b40d3da1c51afa087c66466
Dylan R. <d.raiff@icloud.com>
2020-02-06 13:41:54 -0500

    added implementation for interface Draggable

--- BidimensionalEngine/src/bidimensionalengine/ui/Draggable.java
+++ BidimensionalEngine/src/bidimensionalengine/ui/Draggable.java
@@ -1,10 +1,30 @@
 package bidimensionalengine.ui;
 
+import bidimensionalengine.playercomponents.Transform;
+
 /**
  * @author Dylan Raiff
  * @version 1.0
  */
 public interface Draggable
 {
+	/**
+	 * @return {@code Transform} of the draggable.
+	 */
+	Transform getTransform();
 
+	/**
+	 * Called the frame this object starts to be dragged.
+	 */
+	void onDragStarted();
+
+	/**
+	 * Called during the time this object is being dragged.
+	 */
+	void onDrag();
+
+	/**
+	 * Called the frame this object stops being dragged.
+	 */
+	void onDragEnded();
 }
